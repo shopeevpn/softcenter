67d509e86020000701845aadc1f4826281fe95a7
vpnshopee <90919599+shopeevpn@users.noreply.github.com>
2025-02-02 09:02:57 +0800

    Create push_message.json.js

---
+++ push_message.json.js
@@ -0,0 +1,11 @@
+{
+  "title": "<em><a href='https://www.koolcenter.com/' target='_blank'>Software Center for HND Platform&nbsp;-&nbsp;by&nbsp;koolcenter.com</a></em>",
+  "content1": "<font color='#1E90FF'>Feedback & Communication:&nbsp;&nbsp;</font>\
+  <a href='https://github.com/koolshare/rogsoft' target='_blank'><em>1. Software Center GitHub Project</em></a>&nbsp;&nbsp;&nbsp;&nbsp;\
+  <a href='https://t.me/xbchat' target='_blank'><em>2. Join the Telegram Group</em></a>&nbsp;&nbsp;&nbsp;&nbsp;\
+  <a href='http://shang.qq.com/wpa/qunwpa?idkey=f475468129ba8019245425559b5df5bdad7d7201ac7780417dd0218bbb4e1322' target='_blank'><em>3. Join QQ Group ①</em></a>&nbsp;&nbsp;&nbsp;&nbsp;\
+  <a href='https://qm.qq.com/cgi-bin/qm/qr?k=McCqMKNzfhBSB1Qam1iIvAHEgVhKOlRg&jump_from=webapi' target='_blank'><em>4. Join QQ Group ②</em></a>&nbsp;&nbsp;&nbsp;&nbsp;",
+  "content2": "📌【20240731】Asus RT-BE88U WiFi7 Router Official Modified Firmware 102_33921_alpha_2.0 released. You can now obtain it on the <a href='https://ifdian.net/a/sadog?tab=shop' target='_blank'><b>IFdian Platform by Sponsoring</b></a>",
+  "content3": "📢【20240906】Koolcenter Modified Merlin Firmware Released! Covers 23 Asus models: <a href='https://fw.koolcenter.com/KoolCenter_Merlin_New_Gen_388/' target='_blank'><b>🆓 Free Get 388.6_2</b></a> | <a href='https://ifdian.net/a/sadog?tab=shop' target='_blank'><b>🧧 Sponsor to Get 388.7_0</b></a>",
+  "content4": "<em>📺</em> ChatGPT Plus, Netflix, Disney+, Spotify, YouTube, etc. Account sharing with a discount code: <i>koolcenter</i> for 5% off, <a href='https://nf.video/2m9pJ' target='_blank'>🛒<em>Click to Buy</em>🛒</a>"
+}
